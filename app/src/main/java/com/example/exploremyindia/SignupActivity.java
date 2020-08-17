245246bf88217e3493868c0fc23011dffa79bd5a
Shikhar <f20170794@hyderabad.bits-pilani.ac.in>
2020-08-18 00:55:59 +0530

    Added FirebaseAuth Signup

--- app/src/main/java/com/example/exploremyindia/SignupActivity.java
+++ app/src/main/java/com/example/exploremyindia/SignupActivity.java
@@ -1,19 +1,39 @@
 package com.example.exploremyindia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignupActivity extends AppCompatActivity {
 
     Button gotoSignIn;
+    Button btn_SignUp;
+    EditText txtname;
+    EditText txtemail;
+    EditText txtpasswd;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
 
         gotoSignIn = findViewById(R.id.signin_link_btn);
 
@@ -23,5 +43,59 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(new Intent(SignupActivity.this, LoginActivity.class));
             }
         });
+
+        txtname = findViewById(R.id.name_txt);
+        txtemail = findViewById(R.id.email_txt);
+        txtpasswd = findViewById(R.id.pwd_txt);
+        btn_SignUp = findViewById(R.id.btn_signup);
+
+        btn_SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = txtemail.getText().toString().trim();
+                String passwd = txtpasswd.getText().toString().trim();
+                String name = txtname.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(SignupActivity.this, "Please enter Email", Toast.LENGTH_SHORT).show();
+                }
+
+                if(TextUtils.isEmpty(passwd)){
+                    Toast.makeText(SignupActivity.this, "Please enter a password", Toast.LENGTH_SHORT).show();
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, passwd)
+                        .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignupActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                            .setDisplayName(name).build();
+
+                                    user.updateProfile(profileUpdates)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+
+                                                }
+                                            });
+
+                                } else {
+
+                                    Toast.makeText(SignupActivity.this, "Failed. "+task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+
+
+            }
+        });
+
+
     }
 }
