2b2bb47474b59b0d53fd82294bf892a24149a3ad
Shikhar <f20170794@hyderabad.bits-pilani.ac.in>
2020-08-26 22:26:19 +0530

    Added Autosuggest in City Search.

--- app/src/main/java/com/example/exploremyindia/AddNewTourActivity.java
+++ app/src/main/java/com/example/exploremyindia/AddNewTourActivity.java
@@ -2,22 +2,48 @@ package com.example.exploremyindia;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.textfield.TextInputEditText;
+import com.mmi.services.api.autosuggest.AutoSuggestCriteria;
+import com.mmi.services.api.autosuggest.MapmyIndiaAutoSuggest;
+import com.mmi.services.api.autosuggest.model.AutoSuggestAtlasResponse;
+import com.mmi.services.api.autosuggest.model.ELocation;
+import com.mmi.services.api.textsearch.MapmyIndiaTextSearch;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AddNewTourActivity extends AppCompatActivity {
 
     TextInputEditText mCitySearch;
     Button mNext;
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +55,12 @@ public class AddNewTourActivity extends AppCompatActivity {
         mCitySearch = findViewById(R.id.txt_city_search);
         mNext = findViewById(R.id.btn_city_next);
 
+        handleAutoSuggest();
+
         mNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String city_search = mCitySearch.getText().toString().trim();
+                String city_search = mCitySearch.getText().toString().trim().toLowerCase();
 
                 if(TextUtils.isEmpty(city_search)){
                     Toast.makeText(AddNewTourActivity.this, "Please Enter a City", Toast.LENGTH_SHORT).show();
@@ -43,6 +71,87 @@ public class AddNewTourActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void handleAutoSuggest() {
+
+        recyclerView = findViewById(R.id.city_search_recycler_view);
+        mLinearLayoutManager = new LinearLayoutManager(AddNewTourActivity.this);
+        recyclerView.setLayoutManager(mLinearLayoutManager);
+        recyclerView.setVisibility(View.GONE);
+        handler = new Handler();
+
+        mCitySearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                handler.postDelayed(() -> {
+                    recyclerView.setVisibility(View.GONE);
+                    if (s.length() < 3)
+                        recyclerView.setAdapter(null);
+
+                    if (s != null && s.toString().trim().length() < 2) {
+                        recyclerView.setAdapter(null);
+                        return;
+                    }
+
+                    if (s.length() > 2) {
+                        if (CheckInternet.isNetworkAvailable(AddNewTourActivity.this)) {
+                            callAutoSuggestApi(s.toString());
+                        } else {
+                            //showToast(getString(R.string.pleaseCheckInternetConnection));
+                        }
+                    }
+                }, 300);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void callAutoSuggestApi(String searchString) {
+        MapmyIndiaAutoSuggest.builder()
+                .query(searchString)
+                .pod(AutoSuggestCriteria.POD_CITY)
+                .build()
+                .enqueueCall(new Callback<AutoSuggestAtlasResponse>() {
+                    @Override
+                    public void onResponse(Call<AutoSuggestAtlasResponse> call, Response<AutoSuggestAtlasResponse> response) {
+                        if (response.code() == 200) {
+                            if (response.body() != null) {
+                                ArrayList<ELocation> suggestedList = response.body().getSuggestedLocations();
+                                if (suggestedList.size() > 0) {
+                                    recyclerView.setVisibility(View.VISIBLE);
+                                    AutoSuggestAdapter autoSuggestAdapter = new AutoSuggestAdapter(suggestedList, eLocation -> {
+                                        selectedPlace(eLocation);
+                                        recyclerView.setVisibility(View.GONE);
+                                    });
+                                    recyclerView.setAdapter(autoSuggestAdapter);
+                                }
+                            } else {
+                                //showToast("Not able to get value, Try again.");
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<AutoSuggestAtlasResponse> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+                });
+
+    }
+
+
+    public void selectedPlace(ELocation eloc){
+        mCitySearch.setText(eloc.placeName.toString());
     }
 
     void handleNavMenu(){
--- app/src/main/java/com/example/exploremyindia/ExploreMainActivity.java
+++ app/src/main/java/com/example/exploremyindia/ExploreMainActivity.java
@@ -64,8 +64,6 @@ public class ExploreMainActivity extends AppCompatActivity {
                 nextActivity("mumbai");
             }
         });
-
-
     }
 
     void nextActivity(String city){
